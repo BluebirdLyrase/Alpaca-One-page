77b7cef935579614a2e042cae679c41af98de01e
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-09-26 10:44:17 +0700

    prepare to deploy

--- js/app.js
+++ js/app.js
@@ -266,6 +266,29 @@ document.addEventListener('init', function (event) {
                 // ...
               });
               document.querySelector('ons-navigator').resetToPage('splitter.html');
+
+
+              /////use this after deploy////////
+            //     firebase.auth().signInWithRedirect(provider);
+            //     firebase.auth().getRedirectResult().then(function(result) {
+            //         if (result.credential) {
+            //           // This gives you a Google Access Token. You can use it to access the Google API.
+            //           var token = result.credential.accessToken;
+            //           // ...
+            //         }
+            //         // The signed-in user info.
+            //         var user = result.user;
+            //       }).catch(function(error) {
+            //         // Handle Errors here.
+            //         var errorCode = error.code;
+            //         var errorMessage = error.message;
+            //         // The email of the user's account used.
+            //         var email = error.email;
+            //         // The firebase.auth.AuthCredential type that was used.
+            //         var credential = error.credential;
+            //         // ...
+            //       });
+
         });
 
         $("#regisbtn2").click(function () {
